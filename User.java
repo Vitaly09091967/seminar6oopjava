6377aaffb5e733ce94e94a72ddfa251d0dc66341
Vitaly <1967.maslennikov@gmail.com>
2023-07-18 00:58:39 +0300

    Домашнее задание 6

---
+++ User.java
@@ -0,0 +1,16 @@
+
+public class User {
+    private final String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void report() {
+        System.out.println("Report for user: " + name);
+    }
+}
